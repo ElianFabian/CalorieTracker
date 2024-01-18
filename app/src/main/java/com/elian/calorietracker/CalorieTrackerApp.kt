2833bb45ad08f473e9c0246b559d662d28811405
Elián Fabián <elian.fabian.el.belaizi@gmail.com>
2024-01-18 14:51:15 +0100

    Added TODO

--- app/src/main/java/com/elian/calorietracker/CalorieTrackerApp.kt
+++ app/src/main/java/com/elian/calorietracker/CalorieTrackerApp.kt
@@ -5,6 +5,18 @@ import com.elian.calorietracker.core.util.setupLocale
 import com.elian.calorietracker.di.provideGlobalServices
 import com.zhuinden.simplestack.GlobalServices
 
+/* TODO List:
+    - Implement a detail screen for the tracked food.
+    - Implement allow multiple tracked foods to be added at once.
+    - Implement a screen to edit user information.
+    - Fix ui issues.
+    - Learn about data store migrations.
+    - Improve TrackerOverviewScreen performance.
+    - Implement Timber?
+    - Implement export data functionality?
+    - Implement tests?
+ */
+
 class CalorieTrackerApp : Application() {
 
 	lateinit var globalServices: GlobalServices
